91a3e104b573721246c1b9aa01312a7d43ab1614
Calum Schmidtke <200366773@student.georgianc.on.ca>
2018-12-14 14:31:16 -0500

    Feature #2

--- src/Final/Controller.java
+++ src/Final/Controller.java
@@ -1,9 +1,12 @@
 //Calum Schmidtke - 200366773
 package Final;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,7 +16,7 @@ public class Controller implements Initializable {
     CarLot carLot = new CarLot((CarLoader.loadCars()));
 
     @FXML
-    private TableView<Car> volunteerTable;
+    private TableView<Car> inventoryTable;
     @FXML
     private TableColumn<Car, Integer> yearColumn;
     @FXML
@@ -34,7 +37,21 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        brandComboBox.setPromptText("Select a Genre");
+        brandComboBox.setPromptText("Select Brand");
         brandComboBox.getItems().addAll(carLot.getBrands());
+
+        yearColumn.setCellValueFactory(new PropertyValueFactory<Car, Integer>("year"));
+        brandColumn.setCellValueFactory(new PropertyValueFactory<Car, String>("brand"));
+        modelColumn.setCellValueFactory(new PropertyValueFactory<Car, String>("model"));
+        kmColumn.setCellValueFactory(new PropertyValueFactory<Car, Integer>("kilometer"));
+        priceColumn.setCellValueFactory(new PropertyValueFactory<Car, Double>("price"));
+
+        loadInventory();
+    }
+
+    public void loadInventory() {
+        ObservableList<Car> cars = FXCollections.observableArrayList();
+
+        inventoryTable.getItems().addAll(cars);
     }
 }
